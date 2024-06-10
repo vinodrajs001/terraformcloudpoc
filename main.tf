d0580d59d2d75d79da83d5899d543e0a217107ac
Vinod Shalgar <vinod.shalgar@gmail.com>
2024-06-10 13:11:37 +0530

    chanages

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "github" {
+  owner = "vinodrajs001"
+  repository = "terraformcloudpoc"
+}
+
+data "github_actions_environment_secret" "aws_access_key_id" {
+  environment     = "prod"
+  secret_name     = "AKIAY4M62XUOSWSQUKMJ"
+}
+
+data "github_actions_environment_secret" "aws_secret_access_key" {
+  environment     = "prod"
+  secret_name     = "hE38NoUY8L33tMsudIi298wFik6i3LmRJvLKVdEv"
+}
+
 provider "aws" {
   region = "us-east-1"
   access_key = data.github_actions_environment_secret.aws_access_key_id.secret
