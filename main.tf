19a8c7d9ba559fd2220da0db2796499cbb5b353e
Vinod Shalgar <vinod.shalgar@gmail.com>
2024-06-10 12:22:43 +0530

    cred updated

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAY4M62XUOSWSQUKMJ"
+  secret_key = "hE38NoUY8L33tMsudIi298wFik6i3LmRJvLKVdEv"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0747bdcabd34c712a" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-c2f70dfb-4d1a-40c2-b245-dff0effcfa1a" # Replace with your desired bucket name
+  acl    = "private"               # Access control list (private, public-read, etc.)
 
   tags = {
-    Name = "Example Instance"
+    Name        = "My Bucket"
+    Environment = "Dev"
   }
 }
