4ded24c8404be9ef26124890f84fc184b3df6d42
Vinod Shalgar <vinod.shalgar@gmail.com>
2024-12-17 16:17:08 +0530

    policy testing

--- test001.tf
+++ test001.tf
@@ -1,40 +1,26 @@
-variable "bucket_configs" {
-  type = map(object({    
-    is_javaproj = bool    
-  }))
-  default = {
-    "678c5b75-761f-4ca6-9796-5d911460cd3c-service-001" = {      
-      is_javaproj = false      
-    },
-    "cbe172ae-a153-450a-98aa-0593b24a51cb-service-002" = {      
-      is_javaproj = true      
-    },
-    "33fedfcb-3952-4818-9c9e-992331714002-service-003" = {      
-      is_javaproj = true     
-    }
+# IAM Role
+resource "aws_iam_role" "custom_role" {
+  name = "custom-role"
+  
+  # Reference the trust policy JSON file
+  assume_role_policy = file("${path.module}/trust-policy.json")
+
+  tags = {
+    Environment = "Production"
+    Purpose     = "Custom Role"
   }
 }
 
-
-
-
-# Define a variable to hold the bucket names
-variable "bucket_names" {
-  description = "List of bucket names"
-  type        = list(string)
-  default     = ["678c5b75-761f-4ca6-9796-5d911460cd3c-service-001", "cbe172ae-a153-450a-98aa-0593b24a51cb-service-002", "33fedfcb-3952-4818-9c9e-992331714002-service-003"]
+# IAM Policy
+resource "aws_iam_role_policy" "custom_policy" {
+  name = "custom-policy"
+  role = aws_iam_role.custom_role.id
+  
+  # Reference the role policy JSON file
+  policy = file("${path.module}/role-policy.json")
 }
 
-# Create multiple S3 buckets with tags based on lookup
-resource "aws_s3_bucket" "buckets" {
-  count         = length(var.bucket_names)
-  bucket        = var.bucket_names[count.index]  
-  force_destroy = true
-
-  # Add tags based on the bucket name lookup
-  tags = {
-    Name        = var.bucket_names[count.index]
-    JavaProject = lookup(var.bucket_configs, var.bucket_names[count.index]).is_javaproj ? "true" : "false"
-  }  
-  
+# Output the role ARN
+output "role_arn" {
+  value = aws_iam_role.custom_role.arn
 }
